9bd3d6b1c228facb26c1f074c509ed1672f504ad
Cody Milliron <codymilliron.dev@gmail.com>
2023-05-21 19:55:02 -0400

    Add routing files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const rootDir = require("./util/rootpath.js");
-
+const Router = require("./routes/routes.js")
 
 // App setup
 const PORT = 3000;
@@ -11,12 +11,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(rootDir, "public"))); 
 
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "home.html"))
-}); 
-
-app.get('/users', (req, res, next) => {
-    res.sendFile(path.join(rootDir, "views", "users.html"))
-})
+app.use(Router);
 
 app.listen(PORT, () => console.log(`Node Server listening on ${PORT}`))
